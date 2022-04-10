e6438c4b11af4fcfb2b0f00da6599c973ea73614
XampleV <70326149+XampleV@users.noreply.github.com>
2022-04-10 09:04:47 -0400

    x

--- Networking/Client/client_functions.py
+++ Networking/Client/client_functions.py
@@ -15,11 +15,11 @@ class client_func:
             shutil.copy("""\\\\10.247.71.188\\Deploy\\Firefox Setup 99.0.msi""","C:\\Temp")
             completed = subprocess.run(["powershell", "-Command", "Start-Process 'C:\\Temp\\Firefox Setup 99.0.msi' \"-ms\""], capture_output=True) 
         elif program == "chrome":
-            shutil.copy("\\10.247.71.188\Deploy\thechrome.msi","C:\Temp")
-            completed = subprocess.run(["powershell", "-Command", "Start-Process 'C:\Temp\thechrome.msi' \"/qn\""], capture_output=True)
+            shutil.copy("\\\\10.247.71.188\\Deploy\\thechrome.msi","C:\\Temp")
+            completed = subprocess.run(["powershell", "-Command", "Start-Process 'C:\\Temp\\thechrome.msi' \"/qn\""], capture_output=True)
         elif program == "sublime":
-            shutil.copy("\\10.247.71.188\Deploy\sublime.exe","C:\Temp")
-            completed = subprocess.run(["powershell", "-Command", "Start-Process 'C:\Temp\sublime.exe' \"/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /Sa\""], capture_output=True)
+            shutil.copy("\\\\10.247.71.188\\Deploy\\sublime.exe","C:\\Temp")
+            completed = subprocess.run(["powershell", "-Command", "Start-Process 'C:\\Temp\\sublime.exe' \"/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /Sa\""], capture_output=True)
         elif program == "ipScanner":
-            shutil.copy("\\10.247.71.188\Deploy\ipscanner.exe","C:\Temp")
-            completed = subprocess.run(["powershell", "-Command", "Start-Process 'C:\Temp\ipscanner.exe' \"/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-\""], capture_output=True)
+            shutil.copy("\\\\10.247.71.188\\Deploy\\ipscanner.exe","C:\\Temp")
+            completed = subprocess.run(["powershell", "-Command", "Start-Process 'C:\\Temp\\ipscanner.exe' \"/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-\""], capture_output=True)
